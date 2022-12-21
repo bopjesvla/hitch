9dfe504098f6b01676480a8e8f642913f6572bd6
Bob <negenentwintig@hotmail.com>
2022-12-21 15:42:20 +0100

    fix dump, add installation instructions

--- README.md
+++ README.md
@@ -5,3 +5,16 @@
 - dump.py runs the monthly dump
 - cron.sh is the crontab running above files
 - hitchmap.conf is the NGINX configuration
+
+## Installation
+
+```bash
+pip install numpy pandas folium
+curl https://hitchmap.com/dump.sqlite > points.sqlite
+
+OR
+
+conda install folium
+curl https://hitchmap.com/dump.sqlite > points.sqlite
+```
+
--- dump.py
+++ dump.py
@@ -6,5 +6,6 @@ fn = 'prod-points.sqlite'
 if not os.path.exists(fn):
     exit()
 all_points = pd.read_sql('select * from points where not banned', sqlite3.connect(fn))
+all_points['ip'] = ''
 
-all_points.drop('ip', axis=1).to_sql('points', sqlite3.connect('dump.sqlite'), index=False, if_exists='replace')
+all_points.to_sql('points', sqlite3.connect('dump.sqlite'), index=False, if_exists='replace')
