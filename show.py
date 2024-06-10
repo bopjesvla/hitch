8af1aeb95fde0dad04869fcf8ce7b14bc34c3887
tillwenke <till.wenke@gmx.net>
2024-06-10 20:57:58 +0200

    shorten lines. might add auto formatter later

--- show.py
+++ show.py
@@ -11,8 +11,9 @@ from branca.element import Element
 from string import Template
 import networkx
 
-LIGHT = 'light' in sys.argv
-NEW = 'new' in sys.argv
+LIGHT = "light" in sys.argv
+NEW = "new" in sys.argv
+
 
 def haversine_np(lon1, lat1, lon2, lat2):
     """
@@ -27,38 +28,47 @@ def haversine_np(lon1, lat1, lon2, lat2):
     dlon = lon2 - lon1
     dlat = lat2 - lat1
 
-    a = np.sin(dlat/2.0)**2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon/2.0)**2
+    a = np.sin(dlat / 2.0) ** 2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon / 2.0) ** 2
 
     c = 2 * np.arcsin(np.sqrt(a))
     km = 6367 * c
     # 1.25 because the road distance is, on average, 25% larger than a straight flight
     return 1.25 * km
 
+
 def get_bearing(lon1, lat1, lon2, lat2):
     dLon = lon2 - lon1
     x = np.cos(np.radians(lat2)) * np.sin(np.radians(dLon))
-    y = np.cos(np.radians(lat1)) * np.sin(np.radians(lat2)) - np.sin(np.radians(lat1)) * np.cos(np.radians(lat2)) * np.cos(np.radians(dLon))
-    brng = np.arctan2(x,y)
+    y = np.cos(np.radians(lat1)) * np.sin(np.radians(lat2)) - np.sin(
+        np.radians(lat1)
+    ) * np.cos(np.radians(lat2)) * np.cos(np.radians(dLon))
+    brng = np.arctan2(x, y)
     brng = np.degrees(brng)
 
     return brng
 
-fn = 'prod-points.sqlite' if os.path.exists('prod-points.sqlite') else 'points.sqlite'
-points = pd.read_sql('select * from points where not banned order by datetime is not null desc, datetime desc', sqlite3.connect(fn))
+
+fn = "prod-points.sqlite" if os.path.exists("prod-points.sqlite") else "points.sqlite"
+points = pd.read_sql(
+    "select * from points where not banned order by datetime is not null desc, datetime desc",
+    sqlite3.connect(fn),
+)
 print(f"{len(points)} points currently")
 
-duplicates = pd.read_sql('select * from duplicates where reviewed = accepted', sqlite3.connect(fn))
+duplicates = pd.read_sql(
+    "select * from duplicates where reviewed = accepted", sqlite3.connect(fn)
+)
 
-duplicates['from'] = duplicates[['from_lat', 'from_lon']].apply(tuple, axis=1)
-duplicates['to'] = duplicates[['to_lat', 'to_lon']].apply(tuple, axis=1)
+duplicates["from"] = duplicates[["from_lat", "from_lon"]].apply(tuple, axis=1)
+duplicates["to"] = duplicates[["to_lat", "to_lon"]].apply(tuple, axis=1)
 
-dups = networkx.from_pandas_edgelist(duplicates, 'from', 'to')
+dups = networkx.from_pandas_edgelist(duplicates, "from", "to")
 islands = networkx.connected_components(dups)
 
 replace_map = {}
 
 for island in islands:
-    parents = [node for node in island if node not in duplicates['from'].tolist()]
+    parents = [node for node in island if node not in duplicates["from"].tolist()]
 
     if len(parents) == 1:
         for node in island:
@@ -67,70 +77,122 @@ for island in islands:
 
 print(dups)
 
-points[['lat', 'lon']] = points[['lat', 'lon']].apply(lambda x: replace_map[tuple(x)] if tuple(x) in replace_map else x, axis=1, raw=True)
+points[["lat", "lon"]] = points[["lat", "lon"]].apply(
+    lambda x: replace_map[tuple(x)] if tuple(x) in replace_map else x, axis=1, raw=True
+)
 
 # dups = duplicates.merge(points, left_on='child_id', right_on='id').merge(left_on='parent_id', right_on='id', suffixes=('child_', 'parent_'))
 
-points.loc[points.id.isin(range(1000000,1040000)), 'comment'] = points.loc[points.id.isin(range(1000000,1040000)), 'comment'].str.encode("cp1252",errors='ignore').str.decode('utf-8', errors='ignore')
+points.loc[points.id.isin(range(1000000, 1040000)), "comment"] = (
+    points.loc[points.id.isin(range(1000000, 1040000)), "comment"]
+    .str.encode("cp1252", errors="ignore")
+    .str.decode("utf-8", errors="ignore")
+)
 
 points.datetime = pd.to_datetime(points.datetime)
 
-rads = points[['lon', 'lat', 'dest_lon', 'dest_lat']].values.T
+rads = points[["lon", "lat", "dest_lon", "dest_lat"]].values.T
 
-points['distance'] = haversine_np(*rads)
-points['direction'] = get_bearing(*rads)
+points["distance"] = haversine_np(*rads)
+points["direction"] = get_bearing(*rads)
 
-points.loc[(points.distance<1), 'dest_lat'] = None
-points.loc[(points.distance<1), 'dest_lon'] = None
-points.loc[(points.distance<1), 'direction'] = None
-points.loc[(points.distance<1), 'distance'] = None
+points.loc[(points.distance < 1), "dest_lat"] = None
+points.loc[(points.distance < 1), "dest_lon"] = None
+points.loc[(points.distance < 1), "direction"] = None
+points.loc[(points.distance < 1), "distance"] = None
 
-rounded_dir = 45*np.round(points.direction/45)
-points['arrows'] = rounded_dir.replace({
-    -90: '←', 90: '→', 0: '↑', 180: '↓', -180: '↓', -45: '↖', 45: '↗', 135: '↘', -135: '↙'
-})
+rounded_dir = 45 * np.round(points.direction / 45)
+points["arrows"] = rounded_dir.replace(
+    {
+        -90: "←",
+        90: "→",
+        0: "↑",
+        180: "↓",
+        -180: "↓",
+        -45: "↖",
+        45: "↗",
+        135: "↘",
+        -135: "↙",
+    }
+)
 
-rating_text = 'rating: ' + points.rating.astype(int).astype(str) + '/5'
-destination_text = ', ride: ' + np.round(points.distance).astype(str).str.replace('.0','', regex=False) + ' km ' + points.arrows
+rating_text = "rating: " + points.rating.astype(int).astype(str) + "/5"
+destination_text = (
+    ", ride: "
+    + np.round(points.distance).astype(str).str.replace(".0", "", regex=False)
+    + " km "
+    + points.arrows
+)
 
-points['wait_text'] = None
+points["wait_text"] = None
 has_accurate_wait = ~points.wait.isnull() & ~points.datetime.isnull()
-points.loc[has_accurate_wait, 'wait_text'] = ', wait: ' + points.wait[has_accurate_wait].astype(int).astype(str) + ' min' + (' ' + points.signal[has_accurate_wait].replace({
-    'ask': '💬',
-    'ask-sign': '💬+🪧',
-    'sign': '🪧',
-    'thumb': '👍'
-})).fillna('')
+points.loc[has_accurate_wait, "wait_text"] = (
+    ", wait: "
+    + points.wait[has_accurate_wait].astype(int).astype(str)
+    + " min"
+    + (
+        " "
+        + points.signal[has_accurate_wait].replace(
+            {"ask": "💬", "ask-sign": "💬+🪧", "sign": "🪧", "thumb": "👍"}
+        )
+    ).fillna("")
+)
+
 
 def e(s):
     s2 = s.copy()
-    s2.loc[~s2.isnull()] = s2.loc[~s2.isnull()].map(lambda x: html.escape(x).replace('\n', '<br>'))
+    s2.loc[~s2.isnull()] = s2.loc[~s2.isnull()].map(
+        lambda x: html.escape(x).replace("\n", "<br>")
+    )
     return s2
 
-extra_text = rating_text + points.wait_text.fillna('') + destination_text.fillna('')
 
-comment_nl = points['comment'] + '\n\n'
+extra_text = rating_text + points.wait_text.fillna("") + destination_text.fillna("")
 
-comment_nl.loc[~points.dest_lat.isnull() & points.comment.isnull()] = ''
+comment_nl = points["comment"] + "\n\n"
 
-points['text'] = e(comment_nl) + '<i>' + e(extra_text) + '</i><br><br>―' + e(points['name'].fillna('Anonymous')) + points.datetime.dt.strftime(', %B %Y').fillna('')
+comment_nl.loc[~points.dest_lat.isnull() & points.comment.isnull()] = ""
+
+points["text"] = (
+    e(comment_nl)
+    + "<i>"
+    + e(extra_text)
+    + "</i><br><br>―"
+    + e(points["name"].fillna("Anonymous"))
+    + points.datetime.dt.strftime(", %B %Y").fillna("")
+)
 
 oldies = points.datetime.dt.year <= 2021
-points.loc[oldies, 'text'] = e(comment_nl[oldies]) + '―' + e(points[oldies].name.fillna('Anonymous')) + points[oldies].datetime.dt.strftime(', %B %Y').fillna('')
+points.loc[oldies, "text"] = (
+    e(comment_nl[oldies])
+    + "―"
+    + e(points[oldies].name.fillna("Anonymous"))
+    + points[oldies].datetime.dt.strftime(", %B %Y").fillna("")
+)
 
 # has_text = ~points.text.isnull()
 # points.loc[has_text, 'text'] = points.loc[has_text, 'text'].map(lambda x: html.escape(x).replace('\n', '<br>'))
 
-groups = points.groupby(['lat', 'lon'])
+groups = points.groupby(["lat", "lon"])
 
-places = groups[['country']].first()
-places['rating'] = groups.rating.mean().round()
-places['wait'] = points[~points.wait.isnull()].groupby(['lat', 'lon']).wait.mean()
-places['distance'] = points[~points.distance.isnull()].groupby(['lat', 'lon']).distance.mean()
-places['text'] = groups.text.apply(lambda t: '<hr>'.join(t.dropna()))
-places['review_count'] = groups.size()
-places['dest_lats'] = points.dropna(subset=['dest_lat', 'dest_lon']).groupby(['lat', 'lon']).dest_lat.apply(list)
-places['dest_lons'] = points.dropna(subset=['dest_lat', 'dest_lon']).groupby(['lat', 'lon']).dest_lon.apply(list)
+places = groups[["country"]].first()
+places["rating"] = groups.rating.mean().round()
+places["wait"] = points[~points.wait.isnull()].groupby(["lat", "lon"]).wait.mean()
+places["distance"] = (
+    points[~points.distance.isnull()].groupby(["lat", "lon"]).distance.mean()
+)
+places["text"] = groups.text.apply(lambda t: "<hr>".join(t.dropna()))
+places["review_count"] = groups.size()
+places["dest_lats"] = (
+    points.dropna(subset=["dest_lat", "dest_lon"])
+    .groupby(["lat", "lon"])
+    .dest_lat.apply(list)
+)
+places["dest_lons"] = (
+    points.dropna(subset=["dest_lat", "dest_lon"])
+    .groupby(["lat", "lon"])
+    .dest_lon.apply(list)
+)
 
 if LIGHT:
     places = places[(places.text.str.len() > 0) | ~places.distance.isnull()]
@@ -139,7 +201,7 @@ elif NEW:
 
 places.reset_index(inplace=True)
 # make sure high-rated are on top
-places.sort_values('rating', inplace=True, ascending=False)
+places.sort_values("rating", inplace=True, ascending=False)
 
 m = folium.Map(prefer_canvas=True, control_scale=True, world_copy_jump=True, min_zoom=1)
 
@@ -174,39 +236,86 @@ function (row) {
 """
 
 # for country, group in places.groupby('country_group'):
-cluster = folium.plugins.FastMarkerCluster(places[['lat', 'lon', 'rating', 'text', 'wait', 'distance', 'review_count', 'dest_lats', 'dest_lons']].values, disableClusteringAtZoom=7, spiderfyOnMaxZoom=False, bubblingMouseEvents=False, callback=callback, animate=False).add_to(m)
+cluster = folium.plugins.FastMarkerCluster(
+    places[
+        [
+            "lat",
+            "lon",
+            "rating",
+            "text",
+            "wait",
+            "distance",
+            "review_count",
+            "dest_lats",
+            "dest_lons",
+        ]
+    ].values,
+    disableClusteringAtZoom=7,
+    spiderfyOnMaxZoom=False,
+    bubblingMouseEvents=False,
+    callback=callback,
+    animate=False,
+).add_to(m)
 
 # folium.plugins.Geocoder(position='topleft', add_marker=False, provider='photon').add_to(m)
 
 m.get_root().render()
 
 header = m.get_root().header.render()
-header= header.replace('<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"/>', '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">')
-header= header.replace('<link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css"/>', '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">')
+header = header.replace(
+    '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"/>',
+    '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">',
+)
+header = header.replace(
+    '<link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css"/>',
+    '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">',
+)
 body = m.get_root().html.render()
 script = m.get_root().script.render()
 
-outname = 'light.html' if LIGHT else 'new.html' if NEW else 'index.html'
-template = open('src.html').read()
+outname = "light.html" if LIGHT else "new.html" if NEW else "index.html"
+template = open("src.html").read()
 
-output = Template(template).substitute({
-    'folium_head': header,
-    'folium_body': body,
-    'folium_script': script,
-    'hitch_script': open('map.js').read(),
-    'hitch_style': open('style.css').read()
-})
+output = Template(template).substitute(
+    {
+        "folium_head": header,
+        "folium_body": body,
+        "folium_script": script,
+        "hitch_script": open("map.js").read(),
+        "hitch_style": open("style.css").read(),
+    }
+)
 
-open(outname, 'w').write(output)
+open(outname, "w").write(output)
 
 if not LIGHT:
-    open('lines.html', 'w').write(output)
-    recent = points.dropna(subset=['datetime']).sort_values('datetime',ascending=False).iloc[:1000]
-    recent['url'] = 'https://hitchmap.com/#' + recent.lat.astype(str) + ',' + recent.lon.astype(str)
-    recent['text'] = recent.text.str.replace('://|\n|\r', '', regex=True)
-    recent['name'] = recent.name.str.replace('://', '', regex=False)
-    recent[['url', 'country', 'datetime', 'name', 'rating', 'distance', 'text']].to_html('recent.html', render_links=True, index=False)
+    open("lines.html", "w").write(output)
+    recent = (
+        points.dropna(subset=["datetime"])
+        .sort_values("datetime", ascending=False)
+        .iloc[:1000]
+    )
+    recent["url"] = (
+        "https://hitchmap.com/#" + recent.lat.astype(str) + "," + recent.lon.astype(str)
+    )
+    recent["text"] = recent.text.str.replace("://|\n|\r", "", regex=True)
+    recent["name"] = recent.name.str.replace("://", "", regex=False)
+    recent[
+        ["url", "country", "datetime", "name", "rating", "distance", "text"]
+    ].to_html("recent.html", render_links=True, index=False)
 
-    duplicates['from_url'] = 'https://hitchmap.com/#' + duplicates.from_lat.astype(str) + ',' + duplicates.from_lon.astype(str)
-    duplicates['to_url'] = 'https://hitchmap.com/#' + duplicates.to_lat.astype(str) + ',' + duplicates.to_lon.astype(str)
-    duplicates[['id', 'from_url', 'to_url', 'reviewed', 'accepted']].to_html('recent-dups.html', render_links=True, index=False)
+    duplicates["from_url"] = (
+        "https://hitchmap.com/#"
+        + duplicates.from_lat.astype(str)
+        + ","
+        + duplicates.from_lon.astype(str)
+    )
+    duplicates["to_url"] = (
+        "https://hitchmap.com/#"
+        + duplicates.to_lat.astype(str)
+        + ","
+        + duplicates.to_lon.astype(str)
+    )
+    duplicates[["id", "from_url", "to_url", "reviewed", "accepted"]].to_html(
+        "recent-dups.html", render_links=True, index=False
+    )
