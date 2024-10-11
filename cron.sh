94533cbe54deb6b7f3f404209aee337351627deb
Bob <negenentwintig@hotmail.com>
2024-10-11 11:05:04 +0200

    cron

--- cron.sh
+++ cron.sh
@@ -1,5 +1,5 @@
 @reboot cd hitch && screen -d -m bash -c '. $HOME/.bashrc; /home/bob/.asdf/shims/waitress-serve server:app; bash'
 * * * * * cd hitch && /usr/bin/flock -n /tmp/show.lockfile bash -c '. $HOME/.bashrc; /home/bob/.asdf/shims/python show.py' > cronlog.txt 2>&1
 */10 * * * * cd hitch && /usr/bin/flock -n /tmp/show.lockfile bash -c '. $HOME/.bashrc; /home/bob/.asdf/shims/python show.py light' > cronlog-light.txt 2>&1
-0 0 1 * * cd hitch && /usr/bin/flock -n /tmp/dump.lockfile bash -c '. $HOME/.bashrc; /home/bob/.asdf/shims/python dump.py' > dumplog.txt 2>&1
+0 0 * * * cd hitch && /usr/bin/flock -n /tmp/dump.lockfile bash -c '. $HOME/.bashrc; /home/bob/.asdf/shims/python dump.py' > dumplog.txt 2>&1
 0 0 * * * cd hitch && /usr/bin/flock -n /tmp/dashboard.lockfile bash -c '. $HOME/.bashrc; /home/bob/.asdf/shims/python dashboard.py' > dashboard.txt 2>&1
