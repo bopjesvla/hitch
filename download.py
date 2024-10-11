eb1b2142e0c3547a9698f9779400e336996e9f0e
tillwenke <till.wenke@gmx.net>
2024-10-11 18:19:47 +0200

    rm: download

--- download.py
+++
@@ -1,8 +0,0 @@
-import wget
-import os
-
-url = 'https://hitchmap.com/dump.sqlite'
-filename = 'points.sqlite'
-if os.path.exists(filename):
-        os.remove(filename)
-wget.download(url, filename)
