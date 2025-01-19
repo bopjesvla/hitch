cfadada40c14a5bf640cacb6f1fccfd5668d1122
Bob <negenentwintig@hotmail.com>
2025-01-19 01:19:09 +0100

    final changes

--- dump.py
+++ dump.py
@@ -15,11 +15,3 @@ all_points.to_sql('points', sqlite3.connect(DUMP_DB), index=False, if_exists='re
 duplicates = pd.read_sql('select * from duplicates where reviewed = accepted', sqlite3.connect(fn))
 duplicates["ip"] = ""
 duplicates.to_sql('duplicates', sqlite3.connect(DUMP_DB), index=False, if_exists='replace')
-
-# hitchwiki = pd.read_sql('select * from hitchwiki', sqlite3.connect(fn))
-# hitchwiki["ip"] = ""
-# hitchwiki.to_sql('hitchwiki', sqlite3.connect(DUMP_DB), index=False, if_exists='replace')
-
-users = pd.read_sql('select * from user', sqlite3.connect(fn))
-users = users[["id", "username", "gender", "year_of_birth"]]
-users.to_sql('user', sqlite3.connect(DUMP_DB), index=False, if_exists='replace')
