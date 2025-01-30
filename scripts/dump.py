e28fc2a897503036b0c98369a4c3b1b497f44a79
tillwenke <till.wenke@gmx.net>
2025-01-30 23:06:04 +0100

    feat: make db dump available as excel

--- scripts/dump.py
+++ scripts/dump.py
@@ -11,6 +11,7 @@ os.makedirs(dist_dir, exist_ok=True)
 
 DATABASE = os.path.join(db_dir, "prod-points.sqlite")
 DATABASE_DUMP = os.path.join(dist_dir, "dump.sqlite")
+EXCEL_DUMP = os.path.join(dist_dir, "dump.xlsx")
 
 if not os.path.exists(DATABASE):
     print(f"DB not found: {DATABASE}")
@@ -24,7 +25,6 @@ all_points.to_sql(
     "points", sqlite3.connect(DATABASE_DUMP), index=False, if_exists="replace"
 )
 
-
 duplicates = pd.read_sql(
     "select * from duplicates where reviewed = accepted", sqlite3.connect(DATABASE)
 )
@@ -32,3 +32,7 @@ duplicates["ip"] = ""
 duplicates.to_sql(
     "duplicates", sqlite3.connect(DATABASE_DUMP), index=False, if_exists="replace"
 )
+
+with pd.ExcelWriter(EXCEL_DUMP) as writer:
+    all_points.to_excel(writer, sheet_name="points")  
+    duplicates.to_excel(writer, sheet_name="duplicates")  
