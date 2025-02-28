fb2802f56b53443142e8ba762df4b2e8e727e008
Bob <negenentwintig@hotmail.com>
2025-02-28 20:11:03 +0100

    datasette install script

---
+++ datasette/install.sh
@@ -0,0 +1,5 @@
+datasette install datasette-auth-existing-cookies==1.0a1
+datasette install datasette-write-ui
+datasette install datasette-permissions-sql
+datasette install datasette-events-db
+datasette install datasette-cluster-map
